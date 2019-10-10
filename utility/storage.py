2e892e2753aa5035a509efcd2d19001e3340431a
real-blackshadow <actuallydc@gmail.com>
2019-10-10 22:02:04 +0530

    Collection working by dhanush

--- utility/storage.py
+++ utility/storage.py
@@ -1,13 +1,21 @@
-import sqlite3
 
+import sqlite3
 class Storage:
     def __init__(self,db_name):
         self.db_name = db_name
         self.conn = sqlite3.connect(self.db_name)
+        # self.c = self.conn.cursor()
+    def run_query(self,query,params=None):
+        return (self.conn.execute(query))
         
-    def run_query(self,query):
-        return list(self.conn.execute(query))
 
     def insert_data(self,table_name,data,columns):
-        self.conn.executemany("INSERT OR IGNORE INTO {0} {1} VALUES (?,?)".format(table_name,tuple(columns)),data)
+        print(data,columns)
+        query = "INSERT OR IGNORE INTO {0} {1} VALUES {2}".format(table_name,tuple(columns),
+        ','.join('{}'.format(tuple(each)) for each in data))
+        # print(query)
+        #ignore if data is already present
+        self.conn.execute(query)
         self.conn.commit()
+        
+   
